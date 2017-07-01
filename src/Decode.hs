ec4cb534144fb453f7d2c9a220a11f0e086e1fa6
worker4food <worker4food@users.noreply.github.com>
2017-07-01 14:25:32 +0300

    Remove dependency from attoparsec-binary

--- src/Decode.hs
+++ src/Decode.hs
@@ -1,19 +1,25 @@
 module Decode where
 
-import           Data.Bits
-import           Numeric                         (showHex)
+import           Data.Bits                  (xor)
+import           Numeric                    (showHex)
 
-import           Data.Attoparsec.Binary
-import           Data.Attoparsec.ByteString.Lazy
-import qualified Data.ByteString.Lazy            as B
-import qualified Data.ByteString.Lazy.Char8      as C
-
+import           Data.Binary.Get
+import           Data.ByteString.Lazy       (ByteString)
+import qualified Data.ByteString.Lazy       as B
+import qualified Data.ByteString.Lazy.Char8 as C
 
 key = C.pack $ cycle "19465912879oiuxc ensdfaiuo3i73798kjl"
 
 encode = B.pack . B.zipWith xor key
 decode = encode
 
-mkChecksum = fmap (`showHex` "") . eitherResult . parsed
+mkChecksum :: ByteString -> Either String String
+mkChecksum = extract . runGetOrFail (sumOf 0)
   where
-    parsed = parse (sum <$> many1 anyWord32le <* endOfInput <?> "Broken userdef.usr")
+    extract (Left  _)         = Left "Broken userdef.usr"
+    extract (Right (_, _, x)) = Right $ showHex x ""
+
+    sumOf acc = isEmpty >>= \eof ->
+        if eof
+            then pure acc
+            else getWord32le >>= sumOf . (acc +)

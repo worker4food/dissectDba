cdf0d9f31d409f402dcf66a648602ecadcf0cd24
worker4food <worker4food@users.noreply.github.com>
2017-07-04 10:54:22 +0300

    Some key-val parser refactornig

--- src/PropList.hs
+++ src/PropList.hs
@@ -1,32 +1,29 @@
-module PropList where
+{-# LANGUAGE OverloadedStrings #-}
+module PropList (propList) where
 
 import Control.Applicative ((<|>))
 import Data.Attoparsec.ByteString.Char8
 
--- | Skip all space around specific char
-skipSA c = skipSpace *> char c <* skipSpace
+-- | Skip all space around specific parser
+skipSA p = skipSpace *> p *> skipSpace
 
 quote = char '"'
 
-escEol = (char '\n' <|> char '\r') <* skipSpace <* char '|'
-
 escQuote = quote *> try quote
-
--- | Useless, since escaped eol's not supported
-legalChar = escEol <|> escQuote <|> satisfy (notInClass "\r\n\"")
 
 str = quote *> many' (escQuote <|> notChar '"') <* quote
 
 kv = do
-  _ <- skipSA '{' <?> "pair open"
-  key <- str      <?> "key part"
-  _ <- skipSA ',' <?> "after key part"
-  val <- str      <?> "value part"
-  _ <- skipSA '}' <?> "pair close"
+  skipSA "{" <?> "pair open"
+  key <- str <?> "key part"
+  skipSA "," <?> "after key part"
+  val <- str <?> "value part"
+  skipSA "}" <?> "pair close"
   pure (key, val)
 
+propList :: Parser [(String, String)]
 propList = do
-  _ <- skipSA '{'             <?> "list open"
-  r <- kv `sepBy1` skipSA ',' <?> "list values"
-  _ <- skipSA '}'             <?> "list close"
+  skipSA "{"                  <?> "list open"
+  r <- kv `sepBy1` skipSA "," <?> "list values"
+  skipSA "}"                  <?> "list close"
   pure r
